d5fe80f0ed8644fdb7217f06e320bf02cf3d77bb
Anton Anastasov <anastasovbg@gmail.com>
2013-03-31 15:47:59 -0400

    Added calculation of throughput and latencies to log file.

--- NetworkApp/src/com/example/networkapp/DownloadActivity.java
+++ NetworkApp/src/com/example/networkapp/DownloadActivity.java
@@ -15,111 +15,133 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 
 public class DownloadActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_download);
-		DownloadFile downloadFile = new DownloadFile();
-		downloadFile.execute("http://web.mit.edu/21w.789/www/papers/griswold2004.pdf");
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+        DownloadFile downloadFile = new DownloadFile();
+        downloadFile.execute("http://web.mit.edu/21w.789/www/papers/griswold2004.pdf");
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_download, menu);
-		return true;
-	}
-	
-	private class DownloadFile extends AsyncTask<String, Integer, String> {
-	    @Override
-	    protected String doInBackground(String... sUrl) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_download, menu);
+        return true;
+    }
+
+    private class DownloadFile extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... sUrl) {
             long startTime = System.currentTimeMillis();
             appendLog("New log timestamp at " + Calendar.getInstance().getTime().toGMTString());
-	        try {
-	            URL url = new URL(sUrl[0]);
-	            URLConnection connection = url.openConnection();
-	            connection.connect();
-	            // this will be useful so that you can show a typical 0-100% progress bar
-	            int fileLength = connection.getContentLength();
+            try {
+                URL url = new URL(sUrl[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+                // this will be useful so that you can show a typical 0-100%
+                // progress bar
+                int fileLength = connection.getContentLength();
 
-	            // download the file
-	            InputStream input = new BufferedInputStream(url.openStream());
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream());
+                // overwrite the file since we care only of statistics
                 OutputStream output = new FileOutputStream("sdcard/21w-new.pdf");
 
-                byte data[] = new byte[4096];
-	            long total = 0;
-	            int count;
-	            while ((count = input.read(data)) != -1) {
-	                total += count;
-	                // publishing the progress....
-	                //publishProgress((int) (total * 100 / fileLength));
-	                output.write(data, 0, count);
-                    String logDataString = (System.currentTimeMillis() - startTime) + "ms : "
-                            + total + "/" + fileLength + " bytes.";
-                    Log.i("#", logDataString);
-                    appendLog(logDataString);
-	            }
+                final int MILLISECONDS_IN_PERIOD = 100;
+                final int BYTES_PER_STEP = 512;
+                final double BYTES_IN_KB = 1024.0;
 
-	            output.flush();
-	            output.close();
-	            input.close();
-	            
-	        } catch (Exception e) {
-	        }
-	        return null;
-	    }
-	    @Override
-	    protected void onPreExecute() {
-	        super.onPreExecute();
-	        //mProgressDialog.show();
-	    }
+                byte data[] = new byte[BYTES_PER_STEP];
+                int count;
+                int currentPeriod = 0;
+                long bytesDownloaded = 0;
+                long previousMilliseconds = 0;
+                long previousBytesDownloaded = 0;
+                boolean firstIteration = true;
+                while ((count = input.read(data)) != -1) {
+                    if (firstIteration) {
+                        firstIteration = false;
+                        appendLog("Latency: " + (System.currentTimeMillis() - startTime) + " (ms)");
+                    }
 
-	    @Override
-	    protected void onProgressUpdate(Integer... progress) {
-	        super.onProgressUpdate(progress);
-	        //mProgressDialog.setProgress(progress[0]);
-	    }
-	}
-	    
-	    /**
-		 * Adds to a log file 
-		 * @param text
-		 */
-		public void appendLog(String text)
-		{       
-			//move this to on start
-		   File logFile = new File("sdcard/log.file"); //create log file with date/time
-		   if (!logFile.exists())
-		   {
-		      try
-		      {
-		         logFile.createNewFile();
-		      } 
-		      catch (IOException e)
-		      {
-		         // TODO Auto-generated catch block
-		         e.printStackTrace();
-		      }
-		   }
-		   try
-		   {
-		      //BufferedWriter for performance, true to set append to file flag
-		      BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
-		      buf.append(text);
-		      buf.newLine();
-		      buf.flush();
-		      buf.close();
-		   }
-		   catch (IOException e)
-		   {
-		      // TODO Auto-generated catch block
-		      e.printStackTrace();
-		   }
-		}
+                    bytesDownloaded += count;
+                    long currentTime = System.currentTimeMillis();
+                    if (currentTime - startTime >= (currentPeriod + 1) * MILLISECONDS_IN_PERIOD) {
+                        currentPeriod++;
+                        long milliseconds = (System.currentTimeMillis() - startTime);
+                        String logDataString = (System.currentTimeMillis() - startTime) + "ms : "
+                                + bytesDownloaded + "/" + fileLength + " bytes.";
+                        appendLog(logDataString);
+                        double throughput = ((bytesDownloaded - previousBytesDownloaded) / BYTES_IN_KB)
+                                / ((milliseconds - previousMilliseconds) * 0.001);
+                        appendLog("Throughput last 100 milliseconds: " + throughput + " (KB/s)");
+
+                        previousMilliseconds = milliseconds;
+                        previousBytesDownloaded = bytesDownloaded;
+                    }
+                    // publishing the progress....
+                    // publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+
+                appendLog("Overall average throughput: " + (fileLength / BYTES_IN_KB)
+                        / (0.001 * (System.currentTimeMillis() - startTime)));
+
+                output.flush();
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // mProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+            // mProgressDialog.setProgress(progress[0]);
+        }
+    }
+
+    /**
+     * Adds to a log file
+     * 
+     * @param text
+     */
+    public void appendLog(String text) {
+        // move this to on start
+        File logFile = new File("sdcard/log.file"); // create log file with
+                                                    // date/time
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        try {
+            // BufferedWriter for performance, true to set append to file flag
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            buf.append(text);
+            buf.newLine();
+            buf.flush();
+            buf.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
